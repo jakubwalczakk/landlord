5b0bd75d8790b731fe1ff8e1ce6437861f1db7ef
Jakub Walczak <jakub.tomasz.walczak@gmail.com>
2021-05-10 12:47:16 +0200

    Navbar and favicon modified

--- landlord-web/src/components/topbar/TopBarComponent.tsx
+++ landlord-web/src/components/topbar/TopBarComponent.tsx
@@ -2,29 +2,36 @@ import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import HomeIcon from '@material-ui/icons/Home';
+import Container from '@material-ui/core/Container';
 import AddIcon from '@material-ui/icons/Add';
+import PersonIcon from '@material-ui/icons/Person';
+import {GreenButton} from "../../ui/GreenComponents";
+import {GRAY_COLOR} from "../../COLOR_CONSTANTS";
+import {Typography} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
     },
+    container: {
+        display: 'inline'
+    },
     menuButton: {
-        marginRight: theme.spacing(2),
+        margin: theme.spacing(2),
+        color: '#ffffff'
     },
     title: {
-        flexGrow: 1,
+        display: 'inline',
+        margin: theme.spacing(2),
+        fontSize: '25px'
     },
     navBar: {
-        backgroundColor: '#88898a',
-    }
-    ,
-    addButon: {
-        backgroundColor: '#ff8842',
-        color: 'white',
+        backgroundColor: GRAY_COLOR,
     },
+    logo: {
+        maxWidth: 50,
+    }
 }));
 
 export default function TopBarComponent() {
@@ -33,21 +40,25 @@ export default function TopBarComponent() {
     return (
         <AppBar position={"static"} className={classes.navBar}>
             <Toolbar>
-                {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MenuIcon/>
-                </IconButton> */}
-                <HomeIcon style={{fontSize: 40}}/>
-                <Typography variant="h6" className={classes.title}>
-                    Wynajmowaczka
-                </Typography>
-
+                <Container className={classes.container}>
+                    <img src="logo.png" alt="logo" className={classes.logo}/>
+                    <Typography variant="h6" className={classes.title}>
+                        Wynajmowaczka
+                    </Typography>
+                </Container>
                 <Button
+                    className={classes.menuButton}
+                    startIcon={<PersonIcon/>}
+                >
+                    Moje konto
+                </Button>
+                <GreenButton
                     variant="contained"
-                    color="secondary"
-                    className={classes.addButon}
+                    className={classes.menuButton}
+
                     startIcon={<AddIcon/>}
                 >Dodaj ogłoszenie
-                </Button>
+                </GreenButton>
             </Toolbar>
         </AppBar>
     );
